c73b82294ab222e56e2040f87680d993e840fe8a
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-11 13:36:36 -0700

    added layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,8 @@
 class VisitorsController < ApplicationController
 	def new
-		#Rails.logger.debug 'DEBUG: entering new method'
 		Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
 		@owner = Owner.new
-		#Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-		raise 'Deliberate Failure'
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
 	end
 end
