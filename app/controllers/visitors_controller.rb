be33b5f44f5270836e487d7ac2fe1a1139499be5
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-11 14:16:34 -0700

    front end

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,5 @@ class VisitorsController < ApplicationController
 	def new
 		Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
 		@owner = Owner.new
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:alert] = 'My birthday is soon.'
 	end
 end
